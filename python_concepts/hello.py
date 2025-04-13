3964f6623294bbb449fd13481f48e6d1c2fcb97b
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-04-13 12:21:43 +0530

    add initial implementation of URL shortener with package configuration and dependencies

---
+++ python_concepts/hello.py
@@ -0,0 +1,39 @@
+# print("Hello Guys Nice to meet you")
+
+# if 5 > 16:
+#     print("5 is greater than 16")
+# else:
+#     print("16 is greater than 5")
+
+# name = str(input("Enter your name: "))
+# print("Hello " + name + ", Nice to meet you")
+
+print("Hello", "World", sep="***")
+
+"""
+this is a multiline comment of python.
+this is a multiline comment of python.
+this is a multiline comment of python.
+"""
+# print("Hello World")
+
+para = """There was a king who ruled a kingdom
+He was very kind and just generous to his people.
+He had a beautiful daughter who was very kind and just generous to her people.
+She was very beautiful and had a heart of gold.
+She was very kind and just generous to her people.
+She was very beautiful and had a heart of gold.
+"""
+
+print(len(para))
+
+
+# for x in para:
+#     print(x)
+
+myList = ["apple", "banana", "cherry"]
+myList.append("orange")
+myList.append("kiwi")
+
+print(type(myList))
+print(myList)

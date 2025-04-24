3e6c8ae2769162cb138cace34641128621e60d9e
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-04-24 20:59:19 +0530

    feat: implement CRUD operations for data management in Express server

--- backendHC/express/express_demo.js
+++ backendHC/express/express_demo.js
@@ -2,6 +2,35 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+let data = [];
+let id = 0;
+// add a new data
+app.post("/data", (req, res) => {
+  const { name, designation } = req.body;
+  console.log(name, designation);
+  const newData = { id: ++id, name, designation };
+  data.push(newData);
+  res.status(201).json(newData);
+});
+
+// get all data
+app.get("/data", (req, res) => {
+  res.json(data);
+});
+
+//get one data
+app.get("/data/:id", (req, res) => {
+  const { id } = req.params;
+  const dataItem = data.find((item) => item.id === parseInt(id));
+  if (dataItem) {
+    res.json(dataItem);
+  } else {
+    res.status(404).json({ error: "Data not found" });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -9,6 +38,5 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
 
 export default app;

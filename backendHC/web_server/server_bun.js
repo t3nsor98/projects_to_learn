69ab6b7970d9e90e54eaff8a6d9a772b89aa192a
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-04-24 14:58:15 +0530

    feat: add initial package.json for express backend and create tupels.py for Python concepts
    - Created package.json for the express backend with dependencies.
    - Added tupels.py to demonstrate tuple operations in Python.

--- backendHC/web_server/server_bun.js
+++ backendHC/web_server/server_bun.js
@@ -1,17 +1,16 @@
-import {serve } from "bun"; 
+import { serve } from "bun";
 
 serve({
-    fetch(req) {
-        const url = new URL(req.url);
-        if (url.pathname === "/") {
-            return new Response("Hello, world!", { status: 200 });
-        } else if (url.pathname === "/about") {
-            return new Response("About Us", { status: 200 });
-        } else {
-            return new Response("404 Not Found", { status: 404 });
-        }
-        
-    },
-    port: 3000,
-    hostname: "127.0.0.1"
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") {
+      return new Response("Hello, world!", { status: 200 });
+    } else if (url.pathname === "/about") {
+      return new Response("About Us", { status: 200 });
+    } else {
+      return new Response("404 Not Found", { status: 404 });
+    }
+  },
+  port: 3000,
+  hostname: "127.0.0.1",
 });
---
+++ python_concepts/tupels.py
@@ -0,0 +1,6 @@
+tup1 = ("apple", "banana", "cherry")
+tup2 = (1, 2, 3)
+tup3 = (4, 5, 6)
+
+print(tup1 * 8)  # Concatenation of tuples
+#                 self.food.goto(0, 0)
